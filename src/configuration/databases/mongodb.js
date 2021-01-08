73c2dba98afde7cd73bbc40ac8b1dc14ec1e5c28
leandroandrade <leandro.andrade2588@gmail.com>
2021-01-07 21:49:00 -0300

    refactor: mongodb options

--- src/configuration/databases/mongodb.js
+++ src/configuration/databases/mongodb.js
@@ -1,14 +1,15 @@
 const { MongoClient } = require('mongodb');
 
-const mongoOptions = { useNewUrlParser: true, useUnifiedTopology: true };
-
 const state = {
     db: null,
     client: null,
 };
 
 exports.connect = async () => {
-    const client = await MongoClient.connect(process.env.MONGODB_URL, mongoOptions);
+    const client = await MongoClient.connect(process.env.MONGODB_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    });
 
     state.client = client;
     state.db = client.db(process.env.MONGODB_DB);
