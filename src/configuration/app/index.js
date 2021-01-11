f6ea9193787ecbe9e3d0917a82739fcac362f40b
leandroandrade <leandro.andrade2588@gmail.com>
2021-01-11 07:11:47 -0300

    refactor: some adjusts

--- src/configuration/app/index.js
+++ src/configuration/app/index.js
@@ -1,16 +1,16 @@
 const express = require('express');
 require('express-async-errors');
 
-const index = express();
+const app = express();
 
 const setApp = require('../setup');
 const setHanders = require('../handlers');
 const setRouters = require('../routes');
 const setDocumentation = require('../documentation');
 
-setApp(index);
-setRouters(index);
-setHanders(index);
-setDocumentation(index);
+setApp(app);
+setRouters(app);
+setHanders(app);
+setDocumentation(app);
 
-module.exports = index;
+module.exports = app;
