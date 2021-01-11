035e6c2055003b713f0a00c2eaaa781a2673253d
leandroandrade <leandro.andrade2588@gmail.com>
2021-01-11 20:45:46 -0300

    feat: add date error

--- src/configuration/handlers/response-error-handler.js
+++ src/configuration/handlers/response-error-handler.js
@@ -1,5 +1,5 @@
 module.exports = (err, req, res, next) => {
-    const { statusCode, messages, message } = err;
+    const { statusCode, messages, message, date = new Date() } = err;
 
     if (statusCode && messages) {
         return res.status(statusCode).send({ mensagens: messages });
@@ -7,11 +7,11 @@ module.exports = (err, req, res, next) => {
 
     if (statusCode && message) {
         return res.status(statusCode).send({
-            mensagens: [{ codigo: statusCode, mensagem: message }],
+            mensagens: [{ codigo: statusCode, mensagem: message, date }],
         });
     }
 
     return res.status(500).send({
-        mensagens: messages || [{ codigo: 500, mensagem: 'Ocorreu um erro no servidor.' }],
+        mensagens: messages || [{ codigo: 500, mensagem: 'Ocorreu um erro no servidor.', date }],
     });
 };
