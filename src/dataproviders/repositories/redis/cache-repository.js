e0f313297cf2cf381e0a733a372cce655d21a1e4
leandroandrade <leandro.andrade2588@gmail.com>
2021-01-12 18:20:30 -0300

    feat: add bluebird

--- src/dataproviders/repositories/redis/cache-repository.js
+++ src/dataproviders/repositories/redis/cache-repository.js
@@ -1,5 +1,11 @@
+const Promise = require('bluebird');
+
 const { TEMPO_CACHE_PRODUTO_SEGUNDOS = 120 } = process.env;
 const RedisBD = require('../../../configuration/databases/redis');
+
+Promise.config({
+    cancellation: true,
+});
 
 exports.getCache = async key => RedisBD.client().get(`produtos:${key}`);
 
